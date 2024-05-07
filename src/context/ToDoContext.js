c0d4075dcaccce836b843b13747518860fddb6c7
dxfuryman <94210130+DXFURYMAN@users.noreply.github.com>
2024-05-07 09:09:48 +0530

    dev commit

--- src/context/ToDoContext.js
+++ src/context/ToDoContext.js
@@ -1,5 +1,23 @@
 import {createContext, useContext} from "react";
 
-export const TDcontext = createContext({
+export const Todocontext = createContext({
+todos:[
+    {
+        id:1,
+        todo: "todo message",
+        completed:false,
+    }
+],
+    addTodo: (todo) => {},
+    updatedTodo: (id,todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
 
 })
+
+export const useTodo = () => {
+    return useContext({Todocontext});
+}
+
+export const TodoProvider =  Todocontext.Provider;
+
