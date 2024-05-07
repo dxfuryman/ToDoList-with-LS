c632e9f77785fe547aa94f2828f3c15d429a4ed5
dxfuryman <94210130+DXFURYMAN@users.noreply.github.com>
2024-05-07 09:41:27 +0530

    dev commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,10 @@ const [todos, setTodos] = useState([])
   }
   useEffect(() => {
 const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if (todos && todos.length > 0){
+      setTodos(todos)
+    }
   }, []);
 
   return (
