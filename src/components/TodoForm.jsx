84fc0cdf33df3542e197e217fd944af08b00a673
Vineet Somani <94210130+dxfuryman@users.noreply.github.com>
2024-06-30 04:15:52 +0530

    Update TodoForm.jsx

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -18,7 +18,7 @@ export const TodoForm = () => {
         <form onSubmit={add} className="flex">
             <input
                 type="text"
-                placeholder="Write Todo..."
+                placeholder="Write Todo"
                 className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
                 value={todo}
                 onChange={(e) => setTodo(e.target.value)}
